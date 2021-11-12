04d019752558c6fbd20cc2f4cd74581847b13691
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-12 14:11:45 +0200

    mic update

--- sectiune.php
+++ sectiune.php
@@ -83,7 +83,7 @@ switch ($sect) {
                 <div class="col-md-10 col-lg-8 mx-auto position-relative">
                     <div class="site-heading">
                         <h1>Muzeul de istorie&nbsp;</h1>
-                        <h1>Anton Cincu</h1>
+                        <h1>Teodor Cincu</h1>
                         <span class="subheading">
                             <?php
                             echo $path;
