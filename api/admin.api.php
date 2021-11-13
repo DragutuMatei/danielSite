5d65c0b1a82822e046bd43ddf5d94f03dbf8c7ae
DragutuMatei <67979189+DragutuMatei@users.noreply.github.com>
2021-11-13 20:27:33 +0200

    mesaj

--- api/admin.api.php
+++ api/admin.api.php
@@ -29,21 +29,21 @@ if (isset($_POST['submit'])) {
         $user = new User();
 
         // $files = array_filter($_FILES['imagini']['name']);
-        // $cate_is = count($_FILES['imagini']['name']);
+        $cate_is = count($_FILES['imagini']['name']);
 
-        // $array_cu_imag = array();
+        $array_cu_imag = array();
 
-        // for ($i = 0; $i < $cate_is; $i++) {
-        //     $temporale = $_FILES['imagini']['tmp_name'][$i];
-
+        for ($i = 0; $i < $cate_is; $i++) {
+            // $temporale = $_FILES['imagini']['tmp_name'][$i];
+                $array_cu_imag[$i] = $_FILES['imagini']['name'];
         //     if ($temporale != "") {
         //         $array_cu_imag[$i] = "./assets/img/" . $_FILES['imagini']['name'][$i];
         //         $newFilePath = "../assets/img/" . $_FILES['imagini']['name'][$i];
         //         move_uploaded_file($temporale, $newFilePath);
         //     }
-        // }
+        }
 
-        Input::moveImg("muzeu/", array());
+        Input::moveImg("muzeu/", $array_cu_imag);
 
         try {
             $user->addPost(array(
