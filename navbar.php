0cfcf8b9302295ca59cb24480bbd062e1bb6ca56
ZuZu-Mihail <mihailhanga@gmail.com>
2022-07-13 11:51:53 +0300

    origini

--- navbar.php
+++ navbar.php
@@ -11,82 +11,21 @@
         </a>
         <button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
-            <!-- <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto">
                 <li class="nav-item"><a class="nav-link" href="index.php">Acasa</a></li>
                 <li class="nav-item"><a class="nav-link" href="program.php">Program Vizitare</a></li>
-                <li class="nav-item"><a class="nav-link" href="istoric.php">Istoric</a></li><!-- Start: Sectiuni --
+                <li class="nav-item"><a class="nav-link" href="istoric.php">Istoric</a></li><!-- Start: Sectiuni -->
                 <li class="nav-item">
                 <li class="nav-item dropdown">
                     <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Sectiuni
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=paleontologie">Paleontologie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=arheologie">Arheologie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=numismatica">Numismatică</a>
-                        <a class="dropdown-item" href="colectie.php?sect=istorie">Istorie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=arta">Artă</a>
-                        <a class="dropdown-item" href="colectie.php?sect=educatie">Educație muzeală</a>
-                    </div>
-                </li>
-                </li><!-- End: Sectiuni --
-                <!-- Start: Evenimente --
-                <li class="nav-item">
-                <li class="nav-item dropdown">
-                    <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Evenimente
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=2015">2015</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2016">2016</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2017">2017</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2018">2018</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2019">2019</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2020">2020</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2021">2021</a>
-                    </div>
-                </li>
-                </li><!-- End: Evenimente --
-                <!-- Start: Organizare --
-                <li class="nav-item">
-                <li class="nav-item dropdown">
-                    <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Organizare
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="rof.php">R.O.F.</a>
-                        <a class="dropdown-item" href="organigrama.php">Organigramă</a>
-                        <a class="dropdown-item" href="transparenta.php">Transparență salarială</a>
-                        <a class="dropdown-item" href="buget.php">Buget </a>
-                        <a class="dropdown-item" href="plancultural.php">Plan Cultural</a>
-                    </div>
-                </li>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="tarife.php">Taxe-tarife</a></li>
-                <li class="nav-item">
-                <li class="nav-item dropdown">
-                    <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Arhiva Online
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=publicatii">Publicații tecucene</a>
-                        <a class="dropdown-item" href="colectie.php?sect=materiale">Materiale informative</a>
-                        <a class="dropdown-item" href="colectie.php?sect=diverse">Diverse</a>
-                    </div>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-            </ul> -->
-            <ul class="navbar-nav ms-auto"></ul>
-
-                <li class="nav-item"><a class="nav-link" href="istoric.php">Istoric</a></li><!-- Start: Sectiuni -->
-                <li class="nav-item">
-                <li class="nav-item dropdown">
-                    <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Colectii
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=paleontologie">Paleontologie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=arheologie">Arheologie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=numismatica">Numismatică</a>
-                        <a class="dropdown-item" href="colectie.php?sect=arta">Arta</a>
-                        <a class="dropdown-item" href="colectie.php?sect=etnografie">Etnografie</a>
-                        <a class="dropdown-item" href="colectie.php?sect=donatii">Donatii si donatori</a>
-                        <a class="dropdown-item" href="colectie.php?sect=aumobilul">Aumobilul Ego</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=paleontologie">Paleontologie</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=arheologie">Arheologie</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=numismatica">Numismatică</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=istorie">Istorie</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=arta">Artă</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=educatie">Educație muzeală</a>
                     </div>
                 </li>
                 </li><!-- End: Sectiuni -->
@@ -96,13 +35,13 @@
                     <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Evenimente
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=2015">2015</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2016">2016</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2017">2017</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2018">2018</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2019">2019</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2020">2020</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2021">2021</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2015">2015</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2016">2016</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2017">2017</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2018">2018</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2019">2019</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2020">2020</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=2021">2021</a>
                     </div>
                 </li>
                 </li><!-- End: Evenimente -->
@@ -126,9 +65,9 @@
                     <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Arhiva Online
                     </a>
                     <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=publicatii">Publicații tecucene</a>
-                        <a class="dropdown-item" href="colectie.php?sect=materiale">Materiale informative</a>
-                        <a class="dropdown-item" href="colectie.php?sect=diverse">Diverse</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=publicatii">Publicații tecucene</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=materiale">Materiale informative</a>
+                        <a class="dropdown-item" href="sectiune.php?sect=diverse">Diverse</a>
                     </div>
                 </li>
                 <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
