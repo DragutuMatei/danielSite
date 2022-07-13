e24a2a52c1f6c785fd9a8504f85920caf6638e34
ZuZu-Mihail <mihailhanga@gmail.com>
2022-07-13 11:38:20 +0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -4,23 +4,21 @@
     }
 
     .dropdown-submenu {
-    position: relative;
-  }
-  
-  .dropdown-submenu a::after {
-    transform: rotate(-90deg);
-    position: absolute;
-    right: 6px;
-    top: .8em;
-  }
-  
-  .dropdown-submenu .dropdown-menu {
-    top: 0;
-    left: 100%;
-    margin-top: -1px;
-  }
+        position: relative;
+    }
 
+    .dropdown-submenu a::after {
+        transform: rotate(-90deg);
+        position: absolute;
+        right: 6px;
+        top: .8em;
+    }
 
+    .dropdown-submenu .dropdown-menu {
+        top: 0;
+        left: 100%;
+        margin-top: -1px;
+    }
 </style>
 <nav class="navbar navbar-light navbar-expand-lg fixed-top" style="height: 80px;" id="mainNav">
     <div class="container-fluid">
@@ -119,47 +117,7 @@
                 </ul>
             </li>
             </li><!-- End: Sectiuni -->
-            <!-- Start: Evenimente -->
-            <li class="nav-item">
-            <li class="nav-item dropdown">
-                <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Evenimente
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="colectie.php?sect=2015">2015</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2016">2016</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2017">2017</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2018">2018</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2019">2019</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2020">2020</a>
-                    <a class="dropdown-item" href="colectie.php?sect=2021">2021</a>
-                </div>
-            </li>
-            </li><!-- End: Evenimente -->
-            <!-- Start: Organizare -->
-            <li class="nav-item">
-            <li class="nav-item dropdown">
-                <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Organizare
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="rof.php">R.O.F.</a>
-                    <a class="dropdown-item" href="organigrama.php">Organigramă</a>
-                    <a class="dropdown-item" href="transparenta.php">Transparență salarială</a>
-                    <a class="dropdown-item" href="buget.php">Buget </a>
-                    <a class="dropdown-item" href="plancultural.php">Plan Cultural</a>
-                </div>
-            </li>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="tarife.php">Taxe-tarife</a></li>
-            <li class="nav-item">
-            <li class="nav-item dropdown">
-                <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Arhiva Online
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="colectie.php?sect=publicatii">Publicații tecucene</a>
-                    <a class="dropdown-item" href="colectie.php?sect=materiale">Materiale informative</a>
-                    <a class="dropdown-item" href="colectie.php?sect=diverse">Diverse</a>
-                </div>
-            </li>
+
             <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
             </ul>
         </div>
