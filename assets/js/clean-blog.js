fa7692f9437de7b4885acb96054ff29edc858588
ZuZu-Mihail <mihailhanga@gmail.com>
2022-07-13 11:31:02 +0300

    Update clean-blog.js

--- assets/js/clean-blog.js
+++ assets/js/clean-blog.js
@@ -57,3 +57,15 @@ window.addEventListener('DOMContentLoaded', () => {
       scrollPos = currentTop;
   });
 })
+//https://stackoverflow.com/questions/44467377/bootstrap-4-multilevel-dropdown-inside-navigation
+$('.dropdown-submenu > a').on("click", function(e) {
+    var submenu = $(this);
+    $('.dropdown-submenu .dropdown-menu').removeClass('show');
+    submenu.next('.dropdown-menu').addClass('show');
+    e.stopPropagation();
+});
+
+$('.dropdown').on("hidden.bs.dropdown", function() {
+    // hide any open menus when parent closes
+    $('.dropdown-menu.show').removeClass('show');
+});
