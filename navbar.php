169033615cfad2478198f2b8fc1b16ee8cb40a23
ZuZu-Mihail <mihailhanga@gmail.com>
2022-08-29 01:12:58 +0300

    adaugat ani la evenimente automat

--- navbar.php
+++ navbar.php
@@ -3,52 +3,65 @@
         text-transform: uppercase;
     }
 
-/* ============ desktop view ============ */
-@media all and (min-width: 992px) {
-	.dropdown-menu li{ position: relative; 	}
-	.nav-item .submenu{ 
-		display: none;
-		position: absolute;
-		left:100%; top:-7px;
-	}
-	.nav-item .submenu-left{ 
-		right:100%; left:auto;
-	}
-	.dropdown-menu > li:hover{ background-color: #f1f1f1 }
-	.dropdown-menu > li:hover > .submenu{ display: block; }
-}	
-/* ============ desktop view .end// ============ */
+    /* ============ desktop view ============ */
+    @media all and (min-width: 992px) {
+        .dropdown-menu li {
+            position: relative;
+        }
 
-/* ============ small devices ============ */
-@media (max-width: 991px) {
-  .dropdown-menu .dropdown-menu{
-      margin-left:0.7rem; margin-right:0.7rem; margin-bottom: .5rem;
-  }
-}	
-/* ============ small devices .end// ============ */
+        .nav-item .submenu {
+            display: none;
+            position: absolute;
+            left: 100%;
+            top: -7px;
+        }
 
+        .nav-item .submenu-left {
+            right: 100%;
+            left: auto;
+        }
 
+        .dropdown-menu>li:hover {
+            background-color: #f1f1f1
+        }
 
+        .dropdown-menu>li:hover>.submenu {
+            display: block;
+        }
+    }
+
+    /* ============ desktop view .end// ============ */
+
+    /* ============ small devices ============ */
+    @media (max-width: 991px) {
+        .dropdown-menu .dropdown-menu {
+            margin-left: 0.7rem;
+            margin-right: 0.7rem;
+            margin-bottom: .5rem;
+        }
+    }
+
+    /* ============ small devices .end// ============ */
 </style>
 
 <script src="https://cdn.jsdelivr.net/gh/mickidum/acc_toolbar/acctoolbar/acctoolbar.min.js"></script>
 <script>
-// optional init
-  window.onload = function() {
-    window.micAccessTool = new MicAccessTool({
-      link: 'accessibility.php',
-      contact: 'mailto:muzeultecuci@yahoo.com',
-      buttonPosition: 'right', // default is 'left'
-      forceLang: 'ro-RO' // default is 'en' may be 'he-IL', 'ru-RU', or 'fr_FR'
-    });
-  }
+    // optional init
+    window.onload = function() {
+        window.micAccessTool = new MicAccessTool({
+            link: 'accessibility.php',
+            contact: 'mailto:muzeultecuci@yahoo.com',
+            buttonPosition: 'right', // default is 'left'
+            forceLang: 'ro-RO' // default is 'en' may be 'he-IL', 'ru-RU', or 'fr_FR'
+        });
+    }
 </script>
 
 
 <nav class="navbar navbar-light navbar-expand-lg fixed-top navbar-scroll" id="mainNav">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">
-            <img src="./assets/img/logo_rotund.png" style="width: 80px;" width="80" height="80"  class="d-inline-block align-top" alt="">
+            <img src="./assets/img/logo_rotund.png" style="width: 80px;" width="80" height="80" class="d-inline-block align-top" alt="">
 
         </a>
         <button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
@@ -102,15 +115,23 @@
                 <li class="nav-item dropdown">
                     <a aria-expanded="false" data-bs-toggle="dropdown" class="dropdown-toggle text-uppercase" href="#">Evenimente
                     </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="colectie.php?sect=2015">2015</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2016">2016</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2017">2017</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2018">2018</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2019">2019</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2020">2020</a>
-                        <a class="dropdown-item" href="colectie.php?sect=2021">2021</a>
+                    <div id="an" class="dropdown-menu">
+                        <a class="dropdown-item" href="#">nu s-a putut incarca lista de ani</a>
                     </div>
+
+                    <script>
+                        const an_html_element = document.querySelector("#an");
+                        const an = new Date().getFullYear();
+
+                        an_html_element.innerHTML = "<a class='dropdown-item' href='colectie.php?sect=2015'>2015</a>";
+
+                        for (let i = 2016; i <= an; i++) {
+                            let optionTag = "<a class='dropdown-item' href='colectie.php?sect=" + i + "'>" + i + "</a>";
+                            an_html_element.innerHTML += optionTag;
+                        }
+                    </script>
+
+
                 </li><!-- End: Evenimente -->
 
                 <li class="nav-item"><a class="nav-link" href="program.php">Program Vizitare</a></li>
