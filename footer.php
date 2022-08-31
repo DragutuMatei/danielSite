fc3ac1caf445d069dbf8b7a4fa962296e5311b6d
ZuZu-Mihail <mihailhanga@gmail.com>
2022-08-31 22:19:31 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
             <div class="col-md-10 col-lg-8 mx-auto">
                 <ul class="list-inline text-center">
                     <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCdO2EAqfyx5kx9VJfQjyRaA/videos" class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-youtube fa-stack-1x fa-inverse"></i></a></li>
-                    <li class="list-inline-item"><a href="https://www.facebook.com/muzeultecucean.antoncincu.1" target="_blank" class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.facebook.com/muzeuldeistorietecuci" target="_blank" class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></a></li>
                     <li class="list-inline-item"><a href="https://www.instagram.com/muzeul_teodor_cincu/" target="_blank" class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-instagram fa-stack-1x fa-inverse"></i></a></li>
                 </ul>
                 <p class="text-muted copyright">Copyright&nbsp;©&nbsp;Muzeul Tecuci 2021</p>
