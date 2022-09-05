373c9c3fdcb8bfe3348bb6484c47e34a7ee66ddc
ZuZu-Mihail <mihailhanga@gmail.com>
2022-09-06 01:26:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@ require_once './core/init.php';
                     <div class="muzeu_wrapper">
                         <h4>Edificiul în care funcţionează <strong>Muzeul de Istorie Teodor Cincu</strong> Tecuci , inclus în lista monumentelor istorice, categoria A, a fost construit în mai multe etape începând cu sfârşitul secolului al XIX-lea.</h4>
                         <p>În anul 1885, clădirea este amintită ca fiind în proprietatea lui Anton Cincu, om politic şi filantrop local, care o va lăsa prin testament, la 22 iulie 1894, fiului <span style="color: #333333;">Teodor Cincu</span>. Din punct de vedere arhitectural, se remarcă stilul baroc, predominant în prima etapă a construcţiei,</p>
-                        <p>, când accesul se făcea dinspre sud, un turnuleţ pe care era inserat medalionul cu blazonul familiei străjuind intrarea în casă.</p>
+                        <p>când accesul se făcea dinspre sud, un turnuleţ pe care era inserat medalionul cu blazonul familiei străjuind intrarea în casă.</p>
                         <p>La începutul secolului al XX-lea, s-au modificat faţadele casei, accesul principal fiind orientat spre est şi tratat în stil neoclasic. Un fronton&nbsp; susţinut de patru coloane corintice marchează ….</p>
                     </div>
                 </div>
